64597e791947f566ce4cc67739f3dbe9ddf062e5
joey <joey@spillast>
2017-07-14 12:26:01 -0400

    I forgot to do the plugin install >,<

--- install.sh
+++ install.sh
@@ -11,6 +11,9 @@ fi
 	mv .vimrc $HOME/.vimrc
 	echo "New .vimrc added!"
 
+#installing the plugins	
+echo ":BundleInstall" | vim
+
 echo "Success! vim-Clang will not work unless you have clang installed under"
 echo "/usr/lib/llvm-3.8/lib"
 echo "note: if you're running debian (and possibly ubuntu) you might need to move'libclang-3.8.so.1' to 'libclang.so', because that's what the vim-clang plugin depends on!"
