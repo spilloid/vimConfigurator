327352f22be13d3e39064a9f91446e22d1326f04
Joseph Spillers <jdspille@tesla.cs.iupui.edu>
2017-08-21 12:52:12 -0400

    another small fix. Not working

--- install.sh
+++ install.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 #Install Vundle, plugin manager
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
+if [ -f "$HOME/.vim/"]; then
+	echo "Vundle already installed!"	
+else
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 #Push my custom vimrc into the used vimrc
 if [ -f "$HOME/.vimrc" ]; then
 	echo "Moving old vimrc to .bak"
-	mv .vimrc .vimrc.bak
+	mv $HOME/.vimrc $HOME/.vimrc.bak
 fi
-	mv .vimrc $HOME/.vimrc
+	cp .vimrc $HOME/.vimrc
 	echo "New .vimrc added!"
 
 #installing the plugins	
 echo ":BundleInstall" | vim
-echo "\n\nSucess!\n\n Praise be to the hula-hooping penguins!"
+echo "Sucess! Praise be to the hula-hooping penguins!"
 #echo "Success! vim-Clang will not work unless you have clang installed under" vim-clang removed for compatiblity
 #echo "/usr/lib/llvm-3.8/lib"
 #echo "note: if you're running debian (and possibly ubuntu) you might need to move'libclang-3.8.so.1' to 'libclang.so', because that's what the vim-clang plugin depends on!"
